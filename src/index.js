e794edd93a4de1fc848f296a084b4e171ff98527
Rahemanali-24 <159017790+Rahemanali-24@users.noreply.github.com>
2024-05-15 15:19:27 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+const PORT = 3000;
+app.listen(PORT,function(){
+    console.log(`server is running in port ${PORT}`);
+});
